9f59c2bb882bc6f1b1b73f1d3bb321d37f5897c2
Gajanan-coder-plsql <57040562+Gajanan-coder-plsql@users.noreply.github.com>
2025-12-15 20:04:00 +0530

    Add files via upload

---
+++ Q.SQL.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------
+--  File created - Monday-December-15-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure PROC_STUD_RECORD
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE PROCEDURE "HR"."PROC_STUD_RECORD" IS
+--declare
+    TYPE t1 IS TABLE OF hr.emp3%rowtype  INDEX BY BINARY_INTEGER;
+    v_t t1;
+ BEGIN
+SELECT * BULK COLLECT INTO v_t FROM hr.emp3;
+for i in v_t.first..v_t.last loop
+dbms_output.put_line(v_t(i).first_name||' '||v_t(i).last_name);
+end loop;
+--end;
+END proc_stud_record;
+
+/
